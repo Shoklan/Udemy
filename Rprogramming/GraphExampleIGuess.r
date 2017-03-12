e06a785e12d2c40c9e9b495a50946cc5239334fb
Shoklan <MitColli@corp.suth.com>
2017-03-12 10:24:44 -0400

    Correctly formatting and updating Udemy Repo
    Correctly formatting and updating Udemy Repo

--- Rprogramming/GraphExampleIGuess.r
+++
@@ -1,7 +0,0 @@
-?rivers
-x = 1:141
-y = rivers
-plot(x,y, col = "green", pch= 20,
-     main = "Lengths of \nMajor N. American Rivers.",
-     col.main = "red", xlab = "",
-     ylab = "Length of Rivers")
